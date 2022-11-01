9b97da29387c0268bbaf3babd83f1df9126c83c0
Matheus Assunção <matheus.tba@hotmail.com>
2022-11-01 20:12:28 +0000

    01-11-2022

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,51 +1,16 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+    @inertiaHead
+</head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+<body>
+    @inertia
+</body>
 
-        <style>
-            body {
-                font-family: 'Roboto', sans-serif;
-            }
-        </style>
-
-        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-    </head>
-    <body class="antialiased">
-        <div id="app">
-            <input type="text" v-model="message">
-            <h1>@{{message}}</h1>
-            <ul>
-                <li v-for="friend in friends">
-                    @{{friend.firstName}} @{{friend.surname}}
-                </li>
-            </ul>
-        </div>
-
-
-        <script>
-            var data = {
-                message: 'Hello World',
-                friends: [
-                    {firstName: 'Jane', surname: 'Doe'},
-                    {firstName: 'John', surname: 'Doe'},
-                    {firstName: 'Jack', surname: 'Doe'},
-                ],
-            }
-            
-            const { createApp } = Vue
-
-            createApp({
-                data() {
-                    return data
-                }
-            }).mount('#app')
-        </script>
-    </body>
 </html>
